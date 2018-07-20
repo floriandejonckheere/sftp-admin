9f9639dc6723d78938c64e3fd98d489fb2bf4b93
Florian Dejonckheere <florian@floriandejonckheere.be>
2018-07-20 18:42:01 +0200

    Add KeyManager

---
+++ lib/key_manager.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+class KeyManager
+  @@authorized_keys_path = File.join Rails.application.config.sftp['home_directory'], '.ssh'
+  @@authorized_keys_file = File.join @@authorized_keys_path, 'authorized_keys'
+
+  def self.update_keys
+    File.open @@authorized_keys_file, 'w' do |file|
+      PubKey.all.each do |key|
+        Rails.logger.info "Writing key #{key.id} for user #{key.user.id} to #{@@authorized_keys_file}"
+        file.puts "command=\"#{Rails.root.join('bin', 'sftp-shell')} #{key.user.id}\" #{key.key}"
+      end
+    end
+
+    FileUtils.chmod 0o700, @@authorized_keys_path
+    FileUtils.chmod 0o600, @@authorized_keys_file
+  end
+end
