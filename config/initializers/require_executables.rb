6e3bb98df0eb78272bce25e7bdf103f4d3fff143
Florian Dejonckheere <florian@floriandejonckheere.be>
2018-07-21 15:30:21 +0200

    Require du to be installed

--- config/initializers/require_executables.rb
+++ config/initializers/require_executables.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
+##
+# Require certain executables to be present on the system
+#
+
 # Require fusequota to be installed when quotas are enabled
 if Rails.application.config.sftp['features']['quotas']
   `which fusequota &> /dev/null`
@@ -12,5 +16,10 @@ if Rails.application.config.sftp['features']['encryption']
   abort 'Command not found: encfs. Install encfs or disable encryption in config/sftp.yml.' unless $?.success?
 end
 
+# Require ssh-keygen to be installed to validate OpenSSH keys
 `which ssh-keygen &> /dev/null`
 abort 'Command not found: ssh-keygen. Install ssh-keygen to continue.' unless $?.success?
+
+# Require du to be installed to calculate disk usage
+`which du &> /dev/null`
+abort 'Command not found: du. Install du to continue.' unless $?.success?
