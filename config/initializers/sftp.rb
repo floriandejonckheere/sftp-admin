f8df86691ece416d9e2eb01d791783437238a9a1
Florian Dejonckheere <florian@floriandejonckheere.be>
2018-07-21 15:04:34 +0200

    Require executables to be present at startup

---
+++ config/initializers/sftp.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+# Require fusequota to be installed when quotas are enabled
+if Rails.application.config.sftp['features']['quotas']
+  `which fusequota &> /dev/null`
+  abort 'Command not found: fusequota. Install fusequota or disable quotas in config/sftp.yml.' unless $?.success?
+end
+
+# Require encfs to be installed when encryption is enabled
+if Rails.application.config.sftp['features']['encryption']
+  `which encfs &> /dev/null`
+  abort 'Command not found: encfs. Install encfs or disable encryption in config/sftp.yml.' unless $?.success?
+end
